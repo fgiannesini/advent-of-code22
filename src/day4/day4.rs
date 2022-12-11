0dfffbf30c5d9b9fe8e8aca06331f1167bd42b20
Shiva <fabien.giannesini@gmail.com>
2022-12-11 23:35:06 +0100

    Day 4 part 2

--- src/day4/day4.rs
+++ src/day4/day4.rs
@@ -3,6 +3,7 @@ use crate::utils::file;
 pub fn run() {
     let input = file::read("day4");
     println!("{}", count_included_pairs(input.as_str()));
+    println!("{}", count_overlapped_pairs(input.as_str()));
 }
 
 fn count_included_pairs(input: &str) -> i32 {
@@ -15,14 +16,30 @@ fn is_included(pairs: &str) -> bool {
 }
 
 
+fn count_overlapped_pairs(input: &str) -> i32 {
+    return input.split("\n").filter(|pairs| is_overlapped(pairs.replace('\r', "").as_str())).count() as i32;
+}
+
+fn is_overlapped(pairs: &str) -> bool {
+    let parts: Vec<i32> = pairs.split(&[',', '-']).map(|part| part.parse::<i32>().unwrap()).collect();
+    return parts[0] >= parts[2] && parts[0] <= parts[3] || parts[1] >= parts[2] && parts[1] <= parts[3]
+        || parts[2] >= parts[0] && parts[2] <= parts[1] || parts[3] >= parts[0] && parts[3] <= parts[1]
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day4::day4::count_included_pairs;
+    use crate::day4::day4::{count_included_pairs, count_overlapped_pairs};
     use crate::utils::file;
 
     #[test]
-    fn should_count_score_part_one() {
+    fn should_count_included_part_one() {
         let result = count_included_pairs(file::read_test("day4").as_str());
         assert_eq!(result, 2)
+    }
+
+    #[test]
+    fn should_count_overlapped_part_two() {
+        let result = count_overlapped_pairs(file::read_test("day4").as_str());
+        assert_eq!(result, 4)
     }
 }
