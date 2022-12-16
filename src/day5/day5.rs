c51c4a44215da400b3c78cd54affd0570979b9b2
Fabien Giannesini <fabien.giannesini@gmail.com>
2022-12-16 10:32:30 +0100

    Day 5 part 2

--- src/day5/day5.rs
+++ src/day5/day5.rs
@@ -2,20 +2,21 @@ use crate::utils::file;
 
 pub fn run() {
     let input = file::read("day5");
-    println!("{}", compute_top_of_stacks(input.as_str()));
+    println!("{}", compute_top_of_stacks_with_unordered_moves(input.as_str()));
+    println!("{}", compute_top_of_stacks_with_ordered_moves(input.as_str()));
 }
 
 #[derive(Debug, Clone)]
-struct stack {
+struct Stack {
     values: Vec<String>,
 }
 
 #[derive(Debug, Clone)]
-struct stack_group {
-    values: Vec<stack>,
+struct StackGroup {
+    values: Vec<Stack>,
 }
 
-impl stack {
+impl Stack {
     pub fn new() -> Self {
         Self { values: vec![] }
     }
@@ -30,18 +31,18 @@ impl stack {
     }
 }
 
-impl stack_group {
+impl StackGroup {
     pub fn new(stack_group: &str) -> Self {
         let mut lines: Vec<&str> = stack_group.split("\r\n").collect();
         let stacks_count = lines.pop().unwrap().split_whitespace().count();
         lines.reverse();
-        let mut stacks: Vec<stack> = (0..stacks_count).map(|_| stack::new()).collect();
+        let mut stacks: Vec<Stack> = (0..stacks_count).map(|_| Stack::new()).collect();
         lines.iter().for_each(|line| {
-            let lineElements = Self::split(line);
-            for index in 0..lineElements.len() {
-                let elementValue = lineElements[index].clone();
-                if !elementValue.is_empty() {
-                    stacks[index].add(elementValue)
+            let line_elements = Self::split(line);
+            for index in 0..line_elements.len() {
+                let element_value = line_elements[index].clone();
+                if !element_value.is_empty() {
+                    stacks[index].add(element_value)
                 }
             }
         });
@@ -56,7 +57,15 @@ impl stack_group {
             .collect::<Vec<String>>();
     }
 
-    fn apply_move(&mut self, element_count: i32, origin_index: i32, target_index: i32) {
+    fn apply_move_ordered(&mut self, element_count: i32, origin_index: i32, target_index: i32) {
+        let mut moving_elements = (0..element_count)
+            .map(|_| self.values[origin_index as usize].pop())
+            .collect::<Vec<String>>();
+        moving_elements.reverse();
+        moving_elements.iter().for_each(|element| self.values[target_index as usize].add(element.clone()))
+    }
+
+    fn apply_move_unordered(&mut self, element_count: i32, origin_index: i32, target_index: i32) {
         (0..element_count).for_each(|_| {
             let moving_element = self.values[origin_index as usize].pop();
             self.values[target_index as usize].add(moving_element)
@@ -71,27 +80,46 @@ impl stack_group {
     }
 }
 
-fn compute_top_of_stacks(input: &str) -> String {
+fn compute_top_of_stacks_with_unordered_moves(input: &str) -> String {
     let parts: Vec<&str> = input.split("\r\n\r\n").collect();
     let (stack_group_part, moves) = (parts[0], parts[1]);
-    let mut stack_group = stack_group::new(stack_group_part);
+    let mut stack_group = StackGroup::new(stack_group_part);
     moves.split("\r\n").for_each(|line| {
         let moves_data: Vec<i32> = line.split_whitespace()
             .filter_map(|part| part.parse::<i32>().ok())
             .collect();
-        stack_group.apply_move(moves_data[0], moves_data[1] - 1, moves_data[2] - 1);
+        stack_group.apply_move_unordered(moves_data[0], moves_data[1] - 1, moves_data[2] - 1);
+    });
+    return stack_group.get_tops();
+}
+
+fn compute_top_of_stacks_with_ordered_moves(input: &str) -> String {
+    let parts: Vec<&str> = input.split("\r\n\r\n").collect();
+    let (stack_group_part, moves) = (parts[0], parts[1]);
+    let mut stack_group = StackGroup::new(stack_group_part);
+    moves.split("\r\n").for_each(|line| {
+        let moves_data: Vec<i32> = line.split_whitespace()
+            .filter_map(|part| part.parse::<i32>().ok())
+            .collect();
+        stack_group.apply_move_ordered(moves_data[0], moves_data[1] - 1, moves_data[2] - 1);
     });
     return stack_group.get_tops();
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day5::day5::{compute_top_of_stacks};
+    use crate::day5::day5::{compute_top_of_stacks_with_ordered_moves, compute_top_of_stacks_with_unordered_moves};
     use crate::utils::file;
 
     #[test]
-    fn should_compute_top_of_stacks() {
-        let result = compute_top_of_stacks(file::read_test("day5").as_str());
+    fn should_compute_top_of_stacks_after_unordered_moves() {
+        let result = compute_top_of_stacks_with_unordered_moves(file::read_test("day5").as_str());
         assert_eq!(result, "CMZ")
+    }
+
+    #[test]
+    fn should_compute_top_of_stacks_after_ordered_moves() {
+        let result = compute_top_of_stacks_with_ordered_moves(file::read_test("day5").as_str());
+        assert_eq!(result, "MCD")
     }
 }
